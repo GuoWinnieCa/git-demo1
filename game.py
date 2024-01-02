0887c7b28c927d8a7d3698d010d54506076c4774
winnie <yanweiguo64@gmail.com>
2024-01-02 14:58:08 +0800

    新增game.py區間提示功能

--- game.py
+++ game.py
@@ -1,19 +1,36 @@
 import random
 
-x = random.randint(1, 50)
+# 1~50
+# 使用者猜五次
+# 猜對提前離開
+# 猜錯。提示答案
+
+# 題目
+# (1~50)(1~25)....
+start = 1
+end = 50
+x = random.randint(start, end)
 print(x)
 for i in range(5):
-    y = int(input("請猜數字(1~50):"))
+    y = int(input(f"請猜數字({start}~{end}):"))
     if y == x:
         print("猜對了!")
         break
 
     else:
         if y > x:
+            end = y - 1
             print("猜低一點!!")
+            if end > y:
+                end = y - 1
         else:
+            if start < y:
+                start = y + 1
             print("猜高一點!")
-
+if y != x:
+    print(f"沒有猜對, 正確答案是:{x}")
+else:
+    print(f"恭喜過關,共猜了{i+1}次")
 
 # 1~50
 # 使用者猜五次
