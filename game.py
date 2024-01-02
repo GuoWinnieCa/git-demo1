fce870af621a4f238008bde858a2967bcc0f485a
winnie <yanweiguo64@gmail.com>
2024-01-02 12:58:46 +0800

    1. 刪除原有的git 2. 從新建立git 3.新增game.py猜數字大小提示 4.還進行了程式調整

--- game.py
+++ game.py
@@ -1,15 +1,16 @@
 import random
 
 x = random.randint(1, 50)
-# print(x)
+print(x)
 for i in range(5):
     y = int(input("請猜數字(1~50):"))
     if y == x:
         print("猜對了!")
         break
+
     else:
         if y > x:
-            print("猜低一點!")
+            print("猜低一點!!")
         else:
             print("猜高一點!")
 
